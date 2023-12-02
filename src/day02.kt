692a7253e975d808ba8229f38f3aaeace995148d
Conor Haworth <conor.haworth@awaze.com>
2023-12-02 10:39:32 +0000

    Day 02

---
+++ src/day02.kt
@@ -0,0 +1,32 @@
+fun main() {
+    val input = getInputLines("day02")
+    println("Part 1 answer: ${part1(input)}")
+    println("Part 2 answer: ${part2(input)}")
+}
+
+enum class Colour { RED, GREEN, BLUE }
+
+private fun part1(input: List<String>): Int {
+    return input.fold(0) { acc, line ->
+        if (line.contains("(1[3-9]|[2-9][0-9]) red".toRegex()) ||
+            line.contains("(1[4-9]|[2-9][0-9]) green".toRegex()) ||
+            line.contains("(1[5-9]|[2-9][0-9]) blue".toRegex())
+        ) {
+            acc
+        } else {
+            val id = line.substringBefore(':').substringAfter("Game ").toInt()
+            acc + id
+        }
+    }
+}
+
+private fun part2(input: List<String>): Int {
+    return input.sumOf { line ->
+        Colour.entries.map { line.findMinForColour(it.name.lowercase()) }.reduce(Int::times)
+    }
+}
+
+fun String.findMinForColour(colour: String): Int {
+    val match = Regex("(\\d+ $colour)").findAll(this)
+    return match.map { it.value.substringBefore(" $colour").toInt() }.max()
+}
